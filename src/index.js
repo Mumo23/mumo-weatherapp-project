5a6526249e9276d7adac3ff8baf39760e999eefd
Mumo23 <147050720+Mumo23@users.noreply.github.com>
2023-11-29 14:31:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function displayTemperature(response) {
+function newWeather(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = temperature;
@@ -11,7 +11,7 @@ function search(event) {
   let city = searchInputElement.value;
   let apiKey = "t12ab478ea9e3e17dd09edoc3cbff520";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(newWeather);
 
   cityElement.innerHTML = city;
 }
