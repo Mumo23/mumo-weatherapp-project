f635a38dcfeac6d9a605120963de39b92de36cf4
Mumo23 <147050720+Mumo23@users.noreply.github.com>
2023-11-29 14:15:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-function newWeather(response) {
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
-  let currentDateELement = document.querySelector("#current-date");
-  let cityElement = document.querySelector("#city");
-  let humidityElement = document.querySelector("#humidity");
-  let windspeedElement = document.querySelector("#wind-speed");
-  let emojiElement = document.querySelector("#emoji");
-
-  cityElement.innerHTML = response.data.city;
-  currentDateELement.innerHTML = formatDate(currentDate);
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  temperatureElement.innerHTML = Math.round(temperature);
-  emojiElement.innerHTML = `<img src="${response.data.condition.emoji_url}" class="emoji" />`;
-
-  getForecast(response.data.city);
-}
-
-function searchCity(city) {
-  let apiKey = "t12ab478ea9e3e17dd09edoc3cbff520";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(newWeather);
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+  let humidityElement = document.querySelector("#hmidity");
 }
 
 function search(event) {
   event.preventDefault();
+  let cityElement = document.querySelector("#current-city");
   let searchInputElement = document.querySelector("#search-input");
-  searchCity = searchInputElement.value;
+  let city = searchInputElement.value;
+  let apiKey = "t12ab478ea9e3e17dd09edoc3cbff520";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+
+  cityElement.innerHTML = city;
 }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -42,19 +43,14 @@ function formatDate(date) {
     "Saturday",
   ];
 
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
+let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
 
 function getForecast(city) {
   let apiKey = "t12ab478ea9e3e17dd09edoc3cbff520";
@@ -92,6 +88,4 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHtml;
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", search);
-searchCity();
+displayForecast();
